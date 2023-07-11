d768ebfbe846ba8e86851177b2480f5f98473b71
Hudson Baroni <hudsonbarni@gmail.com>
2023-07-10 22:29:42 -0300

    Tela inicial

---
+++ imhere/src/screens/Home/index.tsx
@@ -0,0 +1,36 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+    function handleParticipantAdd(){
+        return console.log('Participante adicionado');
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text key="1" style={styles.eventName}>
+                Nome do evento
+            </Text>
+
+            <Text style={styles.eventDate}>
+                Segunda-feira, 10 de julho de 2023
+            </Text>
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Nome do participante'
+                    placeholderTextColor={"#6b6b6b"}
+                />
+
+                <TouchableOpacity 
+                style={styles.button}
+                onPress={handleParticipantAdd}
+                >
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
---
+++ imhere/src/screens/Home/styles.ts
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24
+    },
+
+    eventName: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+    },
+
+    eventDate: {
+        color: '#6b6b6b',
+        fontSize: 16,
+        padding: 10,
+        paddingLeft: 0
+    },
+    form:{
+        flexDirection: 'row',
+        width: '100%'
+    },
+    input: {
+        height: 46,
+        flex: 1,
+        marginRight: 5,
+        backgroundColor: '#1f1e25',
+        borderRadius: 6,
+        color: '#fff',
+        paddingLeft: 16,
+        fontSize: 20
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 35,
+    },
+    button: {
+        height:46,
+        width: 46,
+        borderRadius: 10,
+        backgroundColor: '#102536',
+        alignItems: 'center'
+    }
+});
